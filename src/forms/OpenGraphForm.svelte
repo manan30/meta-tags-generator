430fcd27df067a87af65b65dd7e71d613b6c95e5
Manan Joshi <mananjoshi1995@gmail.com>
2020-07-22 11:43:20 -0400

    Adding open graph tags form

--- src/forms/OpenGraphForm.svelte
+++ src/forms/OpenGraphForm.svelte
@@ -1,4 +1,24 @@
-<Input labelFor="Site" required={true} inputType="number" />
+<style>
+  h3 {
+    text-align: center;
+  }
+
+  form {
+    display: grid;
+    gap: 1rem 1rem;
+    grid-template-columns: 1fr 1fr;
+
+    margin-bottom: 1rem;
+  }
+</style>
+
+<h3>Open Graph Tags</h3>
+<form>
+  <Input labelFor="Title" required={true} />
+  <Input labelFor="Type" required={true} />
+  <Input labelFor="Image" required={true} />
+  <Input labelFor="URL" required={true} />
+</form>
 
 <script>
   import Input from '../components/Input.svelte';
